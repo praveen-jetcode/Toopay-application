c6a8a22b4c80bbe72d8efcba9f4fa4435ae401aa
praveen-jetcode <praveen@jetcode.in>
2022-12-01 19:09:16 -0800

    layouts

---
+++ pages/components/Layout.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { useState } from "react";
+import { Logo } from "./Logo";
+import { Sidebar } from "./Sidebar";
+
+export const Layout = ({ children }: any) => {
+  const [open, setOpen] = useState(false);
+  console.log(open, "opennn");
+
+  return (
+    <>
+      <div id="navbar-wrapper">
+        <div className="row">
+          <div className="col-3">
+            <Logo />
+          </div>
+          <div className="col-9">
+            {/* <a href="#" className="navbar-brand" id="sidebar-toggle">
+              <i className="fa fa-bars"></i>
+            </a> */}
+          </div>
+        </div>
+      </div>
+      <div id="wrapper">
+        <Sidebar />
+
+        <section id="content-wrapper">
+          <div className="row">
+            <div className="col-lg-12">{children}</div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+// ///////////////////
+// import Link from "next/link";
+// import { useState } from "react";
+// import { Logo } from "./Logo";
+// import { Sidebar } from "./Sidebar";
+
+// export const Layout = ({ children }: any) => {
+
+//   return (
+//     <>
+//       <div id="navbar-wrapper">
+//         <div className="row">
+//           <div className="col-3">
+//             <Logo />
+//           </div>
+//           <div className="col-8">
+//             {/* <a href="#" className="navbar-brand" id="sidebar-toggle">
+//               <i className="fa fa-bars"></i>
+//             </a> */}
+//           </div>
+//         </div>
+//         <Sidebar />
+//         <section id="content-wrapper">
+//           <div className="row">
+//             <div className="col-lg-12">{children}</div>
+//           </div>
+//         </section>
+//       </div>
+//     </>
+//   );
+// };
