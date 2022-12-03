bf06144fa8720b6c6e53b6fa7330c5035c01e5c1
sivasanmugam-jetcode <sivasanmugam.s@jetcode.in>
2022-12-02 23:52:58 -0800

    alingment changes

--- pages/Report/TdsLetter.tsx
+++ pages/Report/TdsLetter.tsx
@@ -11,11 +11,11 @@ const MerchantCertificate = ()=>{
        
       
           
-              <div className="certificate col-xl-10 m-auto">
-                  <div className="row">
-                    <div className="col-md-12">
+              <div className="certificate ">
+                  {/* <div className="row">
+                    <div className="col-md-12"> */}
                         <div className="row">
-                            <div className="col-md-6 order-2">
+                            <div className="col-lg-12">
                                 <div className="comp-logo">
                                     <img></img>
                                 </div>
@@ -28,16 +28,17 @@ const MerchantCertificate = ()=>{
                                </p>
                             </div>
                         </div>
+                        <div className="row">
                         <div className="col-md-12 certificate-content">
-                           <p style={{fontSize: "15px"}}>Sub: Agent of a corporate/ Principal Business Correspondent TooPay.</p>
-                           <p>Re: Non-Reduction of TDS on Cash Withdrawal over 1 crore as per Section 194N for the financial Year FY 21-22</p>
-                           <p>My/our PAN is <span>N/A</span>My/our PAN is N/A</p>
-                           <p>I/We have been appointed by TooPay. for rendering Business Correspondent services in accordance with the Guidelines issued by the Reserve Bank Of India in this regards by the Reserve Bank of India (RBI) under the Reserve Bank Of India Act 1932 (2 of 1913) to ICICI Bank.</p>
-                           <p>A copy of a valid certificate dated <span>02/01/2020</span>  issued to us by TooPay. is enclosed which is valid as on date.</p>
-                           <p>I/We are maintaining separate bank account number with . from which withdrawal is made only for the “Business Correspondent” activities permitted by the RBI.</p>
-                           <p>I /We hereby agree to indemnify and keep indemnified the Bank against any loss or damages that may be caused to the Bank by reason of incorrect/false information provided by me/us to the Bank and the Bank having acted upon the same. I/We further indemnify keep indemnified the Bank against any and all claims, demands, suits and other proceedings made or instituted by any persons/authorities for or in connection with any matter relating hereto and agree and undertake to pay to Bank on demand all sums of money, costs, charges and expenses incurred, sustained or put to in respect thereof.</p>
-                           <p>Thanking You.</p>
-                           <p className="mt-5">Yours faithfully,</p>
+                           <p className="align">Sub: Agent of a corporate/ Principal Business Correspondent TooPay.</p>
+                           <p className="align">Re: Non-Reduction of TDS on Cash Withdrawal over 1 crore as per Section 194N for the financial Year FY 21-22</p>
+                           <p className="align">My/our PAN is <span>N/A</span>My/our PAN is N/A</p>
+                           <p className="align">I/We have been appointed by TooPay. for rendering Business Correspondent services in accordance with the Guidelines issued by the Reserve Bank Of India in this regards by the Reserve Bank of India (RBI) under the Reserve Bank Of India Act 1932 (2 of 1913) to ICICI Bank.</p>
+                           <p className="align">A copy of a valid certificate dated <span>02/01/2020</span>  issued to us by TooPay. is enclosed which is valid as on date.</p>
+                           <p className="align">I/We are maintaining separate bank account number with . from which withdrawal is made only for the “Business Correspondent” activities permitted by the RBI.</p>
+                           <p className="align">I /We hereby agree to indemnify and keep indemnified the Bank against any loss or damages that may be caused to the Bank by reason of incorrect/false information provided by me/us to the Bank and the Bank having acted upon the same. I/We further indemnify keep indemnified the Bank against any and all claims, demands, suits and other proceedings made or instituted by any persons/authorities for or in connection with any matter relating hereto and agree and undertake to pay to Bank on demand all sums of money, costs, charges and expenses incurred, sustained or put to in respect thereof.</p>
+                           <p className="align">Thanking You.</p>
+                           <p className="mt-5" style={{fontSize:"14px"}}>Yours faithfully,</p>
                            <div className="signature-image">
                             <img className="signatures img-fluid" src="./assets/signature (1).png" style={{width: "15%"}}></img><br></br>
                             <span>
@@ -55,8 +56,9 @@ const MerchantCertificate = ()=>{
                             
                            </div>
                         </div>
-                    </div>
-                  </div>
+                        </div>
+                    {/* </div>
+                  </div> */}
               </div>
           
         
